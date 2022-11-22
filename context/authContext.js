b4b7c5a8fd2a22fccef3d9a2b31f8e6a8e1c8c85
TranPhuongDinh <dinhblade123@gmail.com>
2022-11-22 21:40:17 +0700

    fix code

--- context/authContext.js
+++ context/authContext.js
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { loginFunc, loginGoogleFunc, registerFunc } from "../client/auth";
 import { getUserInfo } from "../client/user";
-import LoadingScreen from "../components/loadingScreen";
+import LoadingScreen from "../components/LoadingScreen";
 
 const AuthContext = createContext();
 
