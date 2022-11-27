6c13a8303c8bcc45851e68c491182434c44b472b
ngoctinvo <ngoctinvo2001@gmail.com>
2022-11-27 10:39:43 +0700

    feat: redirect to login when not authenticated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Dashboard from '../features/Dashboard';
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
 
 const Home = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  if (!isAuthenticated) {
+    window.location.href = "/login";
+    return;
+  }
   return (
     <Dashboard />
   )
