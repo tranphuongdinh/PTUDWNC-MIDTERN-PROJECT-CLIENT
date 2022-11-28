3ae8a9cd9a9b5f441c4b1c90f47c5dbf12c990bb
TranPhuongDinh <dinhblade123@gmail.com>
2022-11-28 22:58:30 +0700

    redirect page after verify

--- pages/active/index.js
+++ pages/active/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { activeAccount } from "../../client/auth";
 
@@ -7,16 +7,22 @@ const ActivePage = () => {
   const router = useRouter();
   const { userId = "", activeCode = "" } = router.query;
 
+  const [isVerifying, setIsVerifying] = useState(false);
+
   const handleActiveAccount = async () => {
     if (userId && activeCode) {
       try {
+        setIsVerifying(true);
         const data = { userId, activeCode };
         const res = await activeAccount(data);
         if (res?.status === "OK") {
           toast.success(res?.message);
+          window.location.href = "/";
         }
+        setIsVerifying(false);
       } catch (e) {
         toast.error(e?.response?.data?.message);
+        setIsVerifying(false);
       }
     }
   };
@@ -25,7 +31,7 @@ const ActivePage = () => {
     handleActiveAccount();
   }, []);
 
-  return <div>Vui lòng kích hoạt tài khoản để sử dụng</div>;
+  return <div>{isVerifying ? "Please wait while we verify your account..." : "We sent a verification link to your email to verify your email address and activate your account."}</div>;
 };
 
 export default ActivePage;
