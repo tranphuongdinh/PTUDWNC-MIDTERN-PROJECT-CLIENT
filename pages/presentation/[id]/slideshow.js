15cc0225fdb57b1d9529d3e197ea513b55e90179
ngoctinvo <ngoctinvo2001@gmail.com>
2022-12-12 20:37:55 +0700

    feat: add slide show view for host and co-host

--- pages/presentation/[id]/slideshow.js
+++ pages/presentation/[id]/slideshow.js
@@ -17,12 +17,64 @@ const SlideShow = () => {
     return res;
   };
   const { id } = router.query;
+  const isOwner = true;
 
   if (!slides.length) {
     return <div>EMPTY SLIDE, CANNOT PRESENT</div>;
   }
   // if (ownerToken === accessToken) { // cho host va co-host
-  if (false) {
+  if (isOwner) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <div style={{ width: "70vw", height: "70vh" }}>
+          current slide show
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            {index > 0 && (
+              <Button onClick={() => setIndex(index - 1)}>Previous</Button>
+            )}
+            {index < slides.length - 1 && (
+              <Button onClick={() => setIndex(index + 1)}>Next</Button>
+            )}
+            <Button
+              onClick={() => (window.location.href = `/presentation/${id}`)}
+            >
+              Exit
+            </Button>
+          </div>
+          <h1>{slides[index]?.content?.question}</h1>
+          <Chart
+            chartType="Bar"
+            width="60vh"
+            height="60vh"
+            data={renderData()}
+          />
+        </div>
+        <div style={{ width: "30vw", height: "30vh" }}>
+          preview next slide
+          {index + 1 < slides.length ? (
+            <>
+              <h1>{slides[index + 1]?.content?.question}</h1>
+              <Chart
+                chartType="Bar"
+                width="60vh"
+                height="60vh"
+                data={renderData()}
+              />
+            </>
+          ) : (
+            <p>da toi cuoi slide</p>
+          )}
+        </div>
+      </div>
+    );
   } else {
     {
       return (
