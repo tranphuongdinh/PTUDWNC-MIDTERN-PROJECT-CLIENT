c82b8c05ba2ef260b8f99fde22678ceff619d380
TranPhuongDinh <dinhblade123@gmail.com>
2022-12-16 20:20:56 +0700

    feat: add multiple choice component

---
+++ components/Presentation/MultipleChoice/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+
+const renderChartData = (options) => {
+  let res = [["Option", "Count"]];
+  options.forEach((option) => res.push([option.label, option.data]));
+  return res;
+};
+
+const MultipleChoicePresentation = (slide) => {
+  const {
+    options = [],
+    question = "",
+    type = "default",
+    width = "100%",
+    height = "100%",
+  } = slide;
+
+  return (
+    <div>
+      {type === "preview" ? <p>{question}</p> : <h2>{question}</h2>}
+      <Chart
+        chartType="Bar"
+        width={width}
+        height={height}
+        data={renderChartData(options)}
+      />
+    </div>
+  );
+};
+
+export default MultipleChoicePresentation;
---
+++ sentry.server.config.js
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+import { SENTRY_DSN } from "./sysconfig";
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
