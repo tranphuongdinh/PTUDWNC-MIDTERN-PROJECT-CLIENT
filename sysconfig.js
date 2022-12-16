82616d2e23a9068ed433cc5f0ccce6757efbffa3
TranPhuongDinh <dinhblade123@gmail.com>
2022-12-16 20:20:56 +0700

    add sentry

--- sysconfig.js
+++ sysconfig.js
@@ -2,3 +2,4 @@ export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 export const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
 export const SERVER_DOMAIN = process.env.NEXT_PUBLIC_SERVER_DOMAIN;
 export const WS_DOMAIN = process.env.NEXT_PUBLIC_WS_DOMAIN
+export const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN
